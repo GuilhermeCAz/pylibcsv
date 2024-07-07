aa4586cbff7e13606c36bc636744058b97cf8a75
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-07-07 19:08:34 -0300

    build: create build shell commands

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-
+apk add --no-cache python3-dev build-base py3-pip
+python3 -m venv /app/venv
+source /app/venv/bin/activate
+pip install --upgrade pip
+pip install cython setuptools
+python3 setup.py build_ext --inplace
+find . -name "libcsv*.so" -exec mv {} libcsv.so \;
