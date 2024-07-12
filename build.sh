03955321278e662550b231060d1643858c3a8e0e
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-07-11 23:18:13 -0300

    style: add minor changes to build.sh commands

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 apk add --no-cache python3-dev build-base py3-pip
-python3 -m venv /app/venv
-source /app/venv/bin/activate
+
+python3 -m venv /app/.venv
+source /app/.venv/bin/activate
+
 pip install --upgrade pip
-pip install cython setuptools
+pip install --no-cache-dir cython setuptools
+
 python3 setup.py build_ext --inplace
-find . -name "libcsv*.so" -exec mv {} libcsv.so \;
+
+mv libcsv*.so libcsv.so
+
+export PYTHONPATH=.
