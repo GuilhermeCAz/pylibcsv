c0204fc9193658a14c49f2d04c4103427817aa9f
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-07-11 23:15:26 -0300

    build: add extension parameters to properly build compatible shared objects with cython

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-import setuptools  # type: ignore[reportMissingImports]
-from Cython.Build import cythonize  # type: ignore[reportMissingImports]
+from Cython.Build import cythonize  # type: ignore[import-untyped]
+from setuptools import Extension, setup
 
-setuptools.setup(
+extensions = [
+    Extension(
+        'libcsv',
+        sources=['src/libcsv.pyx'],
+        include_dirs=['.', '/usr/include/python3.12'],
+        library_dirs=['/usr/lib/python3.12/config-3.12-x86_64-linux-gnu'],
+        libraries=['python3.12'],
+        runtime_library_dirs=['.'],
+        extra_compile_args=['-std=c99'],
+    ),
+]
+
+setup(
+    name='libcsv_wrapper',
     ext_modules=cythonize(
-        setuptools.Extension(
-            'libcsv',
-            sources=['libcsv.pyx'],
-            extra_link_args=['-lpython3.12'],
-        ),
+        extensions,
         language_level=3,
+        force=True,
+        annotate=True,
     ),
     headers=['libcsv.h'],
 )
